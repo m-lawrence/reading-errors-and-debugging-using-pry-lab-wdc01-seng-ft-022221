16d5f65df03985b189557a87866ed4e304c65327
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-19 14:57:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def snake_it_up(string)
   if string.chars[0] == "s"
-    10.times do
-      print "s"
-    end
-    print string
+    ess = "ssssssssss"
+     ess + string
   else
     string
   end
